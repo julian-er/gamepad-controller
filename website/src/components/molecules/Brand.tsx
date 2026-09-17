4bbe2ab9694c0a8015ec24102bbc53c7c0c02f8b
Julian Rosalen <rosalenjulian@gmail.com>
2026-09-17 01:40:38 -0300

    update brand name to include 'ui' in the component

--- website/src/components/molecules/Brand.tsx
+++ website/src/components/molecules/Brand.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '../atoms/Icon';
 
 export function Brand() {
-    return <a className="brand" href="#/"><span className="brand-icon"><Icon /></span><span>gamepad<span className="brand-divider">-</span>controller<span className="brand-dot">.</span></span></a>;
+    return <a className="brand" href="#/"><span className="brand-icon"><Icon /></span><span>gamepad<span className="brand-divider">-</span>ui<span className="brand-divider">-</span>controller<span className="brand-dot">.</span></span></a>;
 }
